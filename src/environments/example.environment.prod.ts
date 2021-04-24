519a914865b4b7c011768f51508388f35017ed8f
Josh Hunter <hunte629@d.umn.edu>
2021-04-23 19:09:35 -0500

    Do what i tried with gitignore but for real this time

---
+++ src/environments/example.environment.prod.ts
@@ -0,0 +1,7 @@
+// Rename this file and fill out the variables below before building for the first time
+
+export const environment = {
+    production: true,
+    strapiURL: "" // Strapi URL goes here 
+  };
+  
